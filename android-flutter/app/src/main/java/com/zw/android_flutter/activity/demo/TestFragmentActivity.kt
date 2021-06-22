22523cb83755e8cfa7c67ef91b3128b73b2ef738
zhouwei441300 <zhouwei441300@163.com>
2021-06-22 20:29:24 +0800

    note

--- android-flutter/app/src/main/java/com/zw/android_flutter/activity/demo/TestFragmentActivity.kt
+++ android-flutter/app/src/main/java/com/zw/android_flutter/activity/demo/TestFragmentActivity.kt
@@ -3,15 +3,41 @@ package com.zw.android_flutter.activity.demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
 import com.zw.android_flutter.R
+import com.zw.android_flutter.fragment.TestFragmentXXX
+import kotlinx.android.synthetic.main.activity_test_fragment.*
 
-class TestFragmentActivity : AppCompatActivity() {
+/**
+ * Adapter对比:
+ * FragmnetPageAdapter在每次切换页面时，只是将Fragment进行分离，
+ * 适合页面较少的Fragment使用以保存一些内存，对系统内存不会多大影响。
+ *
+ * FragmentPageStateAdapter在每次切换页面的时候，是将Fragment进行回收，
+ * 适合页面较多的Fragment使用，这样就不会消耗更多的内存
+ */
+class TestFragmentActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_fragment)
+        btn_test.setOnClickListener(this)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
         return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_test -> {
+                var f = TestFragmentXXX()
+                var b = Bundle()
+                b.putString("Hello", "World")
+                f.arguments = b
+                val ff = supportFragmentManager.beginTransaction()
+                ff.add(R.id.fl_container, f)
+                ff.commit()
+            }
+        }
     }
 }
--- android-flutter/app/src/main/java/com/zw/android_flutter/fragment/TestFragmentXXX.java
+++ android-flutter/app/src/main/java/com/zw/android_flutter/fragment/TestFragmentXXX.java
@@ -3,6 +3,7 @@ package com.zw.android_flutter.fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,22 +15,41 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class TestFragment extends Fragment {
-    public static final String TAG = TestFragment.class.getSimpleName();
+import org.jetbrains.annotations.NotNull;
+
+public class TestFragmentXXX extends Fragment {
+    public static final String TAG = TestFragmentXXX.class.getSimpleName();
 
     private Context context;
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+
+    @Override
+    public void onInflate(@NonNull @NotNull Context context, @NonNull @NotNull AttributeSet attrs, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onInflate(context, attrs, savedInstanceState);
+        Log.i(TAG, "onInflate ......" + context);
+    }
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         this.context = context;
-        Log.i(TAG, "onAttach ......");
+        Log.i(TAG, "onAttach ......" + context);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "onCreate ......");
+        Bundle b = getArguments();
+        String s = null;
+        if (b != null) {
+            s = b.getString("Hello");
+            Log.i(TAG, "onCreate ......");
+        }
+        Log.i(TAG, "onCreate Arguments ......" + s);
     }
 
     @Nullable
