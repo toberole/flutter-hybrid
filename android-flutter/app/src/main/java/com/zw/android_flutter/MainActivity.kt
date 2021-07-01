5b237bb25f5c5dfb275cba3d965724290d482b79
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-07-01 12:57:03 +0800

    note

--- android-flutter/app/src/main/java/com/zw/android_flutter/MainActivity.kt
+++ android-flutter/app/src/main/java/com/zw/android_flutter/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.zw.android_flutter
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.zw.android_flutter.activity.demo.*
 import com.zw.android_flutter.activity.flutter.FlutterActivity1
 import com.zw.android_flutter.activity.flutter.FlutterEngineGroupActivity
 import com.zw.android_flutter.activity.flutter.MethodChannelDemoActivity
+import java.io.File
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        BuildConfig.APPLICATION_ID
 //        // 设置全屏模式
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 //        // 去除标题栏
@@ -98,5 +104,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun getTarget(): Class<*>? {
         return FlutterActivity1::class.java
+    }
+
+
+    private fun installAPK(ctx: Context, apkName: String) {
+        val apkFile = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            apkName
+        )
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val apkUri: Uri = FileProvider.getUriForFile(
+                ctx,
+                ctx.getPackageName().toString() + ".provider",
+                apkFile
+            )
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+        } else {
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive")
+        }
+        ctx.startActivity(intent)
     }
 }
