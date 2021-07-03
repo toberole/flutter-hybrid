0e038afa69229f3497dc93c329f808f2d1307d81
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-07-03 15:57:15 +0800

    note

--- android-flutter/app/src/main/java/com/zw/android_flutter/test1/Test6.java
+++ android-flutter/app/src/main/java/com/zw/android_flutter/test1/Test6.java
@@ -2,7 +2,16 @@ package com.zw.android_flutter.test1;
 
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.WeakHashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Test6 {
     public static final String TAG = Test6.class.getSimpleName();
@@ -32,11 +41,40 @@ public class Test6 {
         map.put("hello1", "world1");
         map.put("hello2", "world1");
 
+        Object o;
+        Callable<String> callable = null;
+        ThreadPoolExecutor threadPoolExecutor = null;
+        Future<String> f = threadPoolExecutor.submit(callable);
+        try {
+            f.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FutureTask<String> futureTask = new FutureTask<>(callable);
+        futureTask = new FutureTask<String>(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, "Hello");
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+
+        CopyOnWriteArrayList<String> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add("hello");
+        String s = copyOnWriteArrayList.get(0);
+        System.out.println(s);
     }
 
     private class TT<T extends Test1> {
         public T getTT(T y) {
             return y;
         }
+    }
+
+    private void test2() {
+        WeakHashMap<String, String> weakHashMap = new WeakHashMap<>();
+        weakHashMap.put("Hello", "World!");
     }
 } 
