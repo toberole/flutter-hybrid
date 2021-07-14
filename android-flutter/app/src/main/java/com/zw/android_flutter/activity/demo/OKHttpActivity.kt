e5a85ed3215ac9f2fe89235fb392fb7ce419f555
zhouwei441300 <zhouwei441300@163.com>
2021-07-14 21:53:33 +0800

    note

---
+++ android-flutter/app/src/main/java/com/zw/android_flutter/activity/demo/OKHttpActivity.kt
@@ -0,0 +1,12 @@
+package com.zw.android_flutter.activity.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zw.android_flutter.R
+
+class OKHttpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_okhttp)
+    }
+}
