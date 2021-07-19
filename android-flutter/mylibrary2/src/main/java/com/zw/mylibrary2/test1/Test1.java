191f20909f21699ca0867d951f4a3494d7e71b05
zhouwei441300 <zhouwei441300@163.com>
2021-07-19 22:32:42 +0800

    lifecycle

--- android-flutter/mylibrary2/src/main/java/com/zw/mylibrary2/test1/Test1.java
+++ android-flutter/mylibrary2/src/main/java/com/zw/mylibrary2/test1/Test1.java
@@ -1,6 +1,14 @@
 package com.zw.mylibrary2.test1;
 
 import android.util.LruCache;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class Test1 {
     public void test1() {
@@ -17,5 +25,41 @@ public class Test1 {
         };
 
         lruCache.put("", "");
+    }
+
+    public void test2() {
+        // https://www.jianshu.com/p/f592f3715ae2
+        RecyclerView recyclerView = null;
+        recyclerView.setAdapter(new MyAdapter());
+
+        ListView listView = null;
+        listView.setAdapter(null);
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<VH> {
+
+        @NonNull
+        @NotNull
+        @Override
+        public VH onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull @NotNull VH holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    private class VH extends RecyclerView.ViewHolder {
+
+        public VH(@NonNull @NotNull View itemView) {
+            super(itemView);
+        }
     }
 } 
